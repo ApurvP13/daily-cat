1ec6e25d0206003896fbb9fad6cac0b6de3f3550
Apurv Pandey <apurvpandey1306@gmail.com>
2025-12-06 21:02:22 +0530

    added protected routing

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="w-full h-full flex items-center justify-center text-4xl font-bold">
+      Dashboard (Protected)
+    </div>
+  );
+}
